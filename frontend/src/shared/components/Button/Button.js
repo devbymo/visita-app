5f8f5f3921b2517f1474066723a16d0969df8ea5
Mo Yasser <alalmy475@gmail.com>
2022-05-20 19:18:21 +0200

    Update the button commponent to be more genaric

--- frontend/src/shared/components/Button/Button.js
+++ frontend/src/shared/components/Button/Button.js
@@ -1,45 +1,128 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-import './Button.css';
+const StyleButton = styled.div`
+  .button {
+    font-size: ${(props) => props.fontSize || '1.8rem'};
+    font-weight: 500;
+    padding: 1.2rem 2.5rem;
+    /* border: 1px solid rgba(0, 0, 0, 0.7); */
+    border: none;
+    border-radius: 0.6rem;
+    background-color: ${(props) => props.buttonBackgroundColor || '#2185d0'};
+    color: ${(props) => props.buttonTextColor || '#fff'};
+    cursor: pointer;
+    margin-right: 1rem;
+    text-decoration: none;
+    display: inline-block;
+  }
+
+  .button:focus {
+    outline: none;
+  }
+
+  .button:hover,
+  .button:active {
+    background-color: ${(props) =>
+      props.buttonBackgroundColorHover || '#1e73be'};
+    color: ${(props) => props.buttonTextColorHover || '#fff'};
+    /* background: #409ee6; */
+  }
+
+  .button--inverse {
+    background: transparent;
+    color: #000;
+  }
+
+  .button--inverse:hover,
+  .button--inverse:active {
+    color: white;
+    background: salmon;
+  }
+
+  .button--danger {
+    background: #830000;
+    border-color: #830000;
+  }
+
+  .button--danger:hover,
+  .button--danger:active {
+    background: #f34343;
+    border-color: #f34343;
+  }
+
+  .button:disabled,
+  .button:hover:disabled,
+  .button:active:disabled {
+    background: #ccc;
+    color: #979797;
+    border-color: #ccc;
+    cursor: not-allowed;
+  }
+`;
 
 const Button = (props) => {
   if (props.href) {
     return (
-      <a
-        className={`button button--${props.size || 'default'} ${
-          props.inverse && 'button--inverse'
-        } ${props.danger && 'button--danger'}`}
-        href={props.href}
+      <StyleButton
+        buttonBackgroundColor={props.buttonBackgroundColor}
+        buttonBackgroundColorHover={props.buttonBackgroundColorHover}
+        buttonTextColor={props.buttonTextColor}
+        buttonTextColorHover={props.buttonTextColorHover}
+        buttonFontSize={props.buttonFontSize}
       >
-        {props.children}
-      </a>
+        <a
+          className={`button button--${props.size || 'default'} ${
+            props.inverse && 'button--inverse'
+          } ${props.danger && 'button--danger'}`}
+          href={props.href}
+        >
+          {props.children}
+        </a>
+      </StyleButton>
     );
   }
   if (props.to) {
     return (
-      <Link
-        to={props.to}
-        exact={props.exact}
-        className={`button button--${props.size || 'default'} ${
-          props.inverse && 'button--inverse'
-        } ${props.danger && 'button--danger'}`}
+      <StyleButton
+        buttonBackgroundColor={props.buttonBackgroundColor}
+        buttonBackgroundColorHover={props.buttonBackgroundColorHover}
+        buttonTextColor={props.buttonTextColor}
+        buttonTextColorHover={props.buttonTextColorHover}
+        buttonFontSize={props.buttonFontSize}
       >
-        {props.children}
-      </Link>
+        <Link
+          to={props.to}
+          exact={props.exact}
+          className={`button button--${props.size || 'default'} ${
+            props.inverse && 'button--inverse'
+          } ${props.danger && 'button--danger'}`}
+        >
+          {props.children}
+        </Link>
+      </StyleButton>
     );
   }
   return (
-    <button
-      className={`button button--${props.size || 'default'} ${
-        props.inverse && 'button--inverse'
-      } ${props.danger && 'button--danger'}`}
-      type={props.type}
-      onClick={props.onClick}
-      disabled={props.disabled}
+    <StyleButton
+      buttonBackgroundColor={props.buttonBackgroundColor}
+      buttonBackgroundColorHover={props.buttonBackgroundColorHover}
+      buttonTextColor={props.buttonTextColor}
+      buttonTextColorHover={props.buttonTextColorHover}
+      buttonFontSize={props.buttonFontSize}
     >
-      {props.children}
-    </button>
+      <button
+        className={`button button--${props.size || 'default'} ${
+          props.inverse && 'button--inverse'
+        } ${props.danger && 'button--danger'}`}
+        type={props.type}
+        onClick={props.onClick}
+        disabled={props.disabled}
+      >
+        {props.children}
+      </button>
+    </StyleButton>
   );
 };
 
