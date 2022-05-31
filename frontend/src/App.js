e34b3b1a53ddecf75380167f517518c5b6e7d169
Mo Yasser <alalmy475@gmail.com>
2022-05-31 22:57:01 +0200

    Adding the update place page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import TestComponent from './user/components/TestComponent';
 import NotFound from './shared/components/NotFound/NotFound';
 import UserPlaces from './place/pages/UserPlaces';
 import NewPlace from './place/pages/NewPlace';
+import UpdatePlace from './place/pages/UpdatePlace';
 
 function App() {
   return (
@@ -14,18 +15,27 @@ function App() {
         <Navbar />
       </header>
       <Switch>
+        {/* The root page which display all users */}
         <Route path="/" exact>
           <Users />
         </Route>
+        {/* Display all users */}
         <Route path="/users" exact>
           <Redirect to="/" />
         </Route>
+        {/* Display user places */}
         <Route path="/:userId/places" exact>
           <UserPlaces />
         </Route>
+        {/* Create new place */}
         <Route path="/places/new" exact>
           <NewPlace />
         </Route>
+        {/* Update The place */}
+        <Route path="/places/:placeId" exact>
+          <UpdatePlace />
+        </Route>
+        {/* Page not found error */}
         <Route path="*">
           <NotFound />
         </Route>
