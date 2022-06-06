0206b78c93644eee7490df3d9f03debf6cf4d39b
Mo Yasser <alalmy475@gmail.com>
2022-06-06 23:25:04 +0200

    Adding Context wide state to the root and refactoring our routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState, useCallback } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import Users from './user/pages/Users';
 import Navbar from './shared/components/Navigation/Navbar';
@@ -7,13 +8,26 @@ import UserPlaces from './place/pages/UserPlaces';
 import NewPlace from './place/pages/NewPlace';
 import UpdatePlace from './place/pages/UpdatePlace';
 import Auth from './user/pages/Auth';
+import { AuthContext } from './shared/context/auth-context';
 
 function App() {
-  return (
-    <div className="app">
-      <header>
-        <Navbar />
-      </header>
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  // const [userId, setUserId] = useState(null);
+
+  const login = useCallback((uid) => {
+    setIsAuthenticated(true);
+    // setUserId(uid);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsAuthenticated(false);
+    // setUserId(null);
+  }, []);
+
+  let routes;
+
+  if (isAuthenticated) {
+    routes = (
       <Switch>
         {/* The root page which display all users */}
         <Route path="/" exact>
@@ -40,11 +54,75 @@ function App() {
           <Auth />
         </Route>
         {/* Page not found error */}
-        <Route path="*">
+        {/* <Route path="*">
           <NotFound />
-        </Route>
+        </Route> */}
+        {/* The default page */}
+        <Redirect to="/" />
       </Switch>
-    </div>
+    );
+  } else {
+    routes = (
+      <Switch>
+        {/* The root page which display all users */}
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        {/* Display all users */}
+        <Route path="/users" exact>
+          <Redirect to="/" />
+        </Route>
+        {/* Display user places */}
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        {/* Auth page */}
+        <Route path="/auth" exact>
+          <Auth />
+        </Route>
+        {/* Page not found error */}
+        {/* <Route path="*">
+          <NotFound />
+        </Route> */}
+        <Redirect to="/" />
+      </Switch>
+    );
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ isAuthenticated: isAuthenticated, login: login, logout: logout }}
+    >
+      <div className="app">
+        <header>
+          <Navbar />
+        </header>
+        <main>{routes}</main>
+        {/* <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/users" exact>
+            <Redirect to="/" />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Route path="/:creatorId/places/:placeId" exact>
+            <UpdatePlace />
+          </Route>
+          <Route path="/auth" exact>
+            <Auth />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch> */}
+      </div>
+    </AuthContext.Provider>
   );
 }
 
