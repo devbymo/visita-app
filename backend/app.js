58f4819dfd2dc5ab19554a437c600f73d6995ffe
Mo Yasser <alalmy475@gmail.com>
2022-06-10 23:12:29 +0200

    Adding the basic express server

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+// create basic express app
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
