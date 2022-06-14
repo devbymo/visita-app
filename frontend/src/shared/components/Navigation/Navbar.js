da878ebcc441516aa2039038f58f0435b15cd342
Mo Yasser <alalmy475@gmail.com>
2022-06-14 23:08:10 +0200

    importing styled navbar as seperated component

--- frontend/src/shared/components/Navigation/Navbar.js
+++ frontend/src/shared/components/Navigation/Navbar.js
@@ -1,97 +1,9 @@
 import React, { useState, useContext } from 'react';
 import { FiMenu, FiX } from 'react-icons/fi';
 import { NavLink } from 'react-router-dom';
-import styled from 'styled-components';
 import Overlay from '../UI/Overlay';
 import { AuthContext } from '../../context/auth-context';
-
-const StyledNavbar = styled.nav`
-  height: 80px;
-  width: 100%;
-  background: #334155;
-  color: white;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 4rem;
-  font-size: 2.4rem;
-  position: fixed;
-  z-index: 10000;
-
-  .nav-logo {
-    font-size: 4rem;
-    font-weight: 500;
-    text-decoration: none;
-    color: white;
-  }
-
-  .nav-item:not(:last-child) {
-    padding-right: 4rem;
-  }
-
-  .nav-links {
-    display: grid;
-    grid-template-columns: repeat(4, auto);
-    grid-gap: 20px;
-    list-style: none;
-  }
-
-  .nav-link {
-    text-decoration: none;
-    color: white;
-    transition: 0.2s all;
-  }
-
-  .nav-link:hover {
-    color: salmon;
-  }
-
-  .nav-link-active {
-    color: salmon;
-  }
-
-  .nav-icon {
-    display: none;
-    font-size: 4rem;
-    cursor: pointer;
-  }
-
-  @media only screen and (max-width: 950px) {
-    position: fixed;
-
-    .nav-links {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      flex-direction: column;
-      position: absolute;
-      width: 100%;
-      top: 80px;
-      left: -100%;
-      transition: 0.5s all;
-    }
-
-    .nav-links.active {
-      background: #334155;
-      left: 0;
-    }
-
-    .nav-links.active .nav-item {
-      text-align: center;
-      padding: 0;
-      width: 80%;
-      padding-bottom: 2rem;
-    }
-
-    .nav-links.active .nav-item:not(:last-child) {
-      border-bottom: 1px solid #777;
-    }
-
-    .nav-icon {
-      display: flex;
-    }
-  }
-`;
+import StyledNavbar from './StyledNavbar';
 
 const Navbar = () => {
   const { isAuthenticated } = useContext(AuthContext);
@@ -107,7 +19,7 @@ const Navbar = () => {
   };
 
   return (
-    <StyledNavbar className="navbar">
+    <StyledNavbar>
       <Overlay show={open} closeMenu={closeMenu} />
       <NavLink to="/" className="nav-logo">
         Visita
