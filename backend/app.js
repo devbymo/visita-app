f60d3c9716cc9fdb73a7377d7fc693b05c7d7d20
Mohamed475 <alalmy475@gmail.com>
2022-07-01 18:10:07 +0200

    Adding the need 3rd-party libraries

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,18 @@
-// create basic express app
-const express = require('express');
+// Adding the need 3rd-party libraries
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-
-app.listen(3000, () => {
-  console.log('listening on port 3000');
-});
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+const config = require("./config/database");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const expressJWT = require("express-jwt");
+const multer = require("multer");
+const fs = require("fs");
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerGridfs = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
