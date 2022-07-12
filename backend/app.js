5108289ff466db90e2524c42d0897d6138da728f
Mo Yasser <alalmy475@gmail.com>
2022-07-12 14:42:15 +0200

    dev

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,1 @@
-// Adding the need 3rd-party libraries
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const path = require("path");
-const config = require("./config/database");
-const passport = require("passport");
-const jwt = require("jsonwebtoken");
-const expressJWT = require("express-jwt");
-const multer = require("multer");
-const fs = require("fs");
-const multerS3 = require("multer-s3");
-const aws = require("aws-sdk");
-const multer = require("multer");
-const multerGridfs = require("multer-gridfs-storage");
-const Grid = require("gridfs-stream");
+
