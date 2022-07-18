86c096087ebeff60c9a695586c182436b3e2c626
Mo Yasser <alalmy475@gmail.com>
2022-07-18 22:44:04 +0200

    Seperating the controllers and adding genaric error handling middleware

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,23 @@ app.use('/api/v1/places', placesRoutes);
 // Users routes.
 app.use('/api/v1/users', usersRoutes);
 
+// Genaric error handler middleware.
+app.use((error, req, res, next) => {
+  console.log('Error handler middleware..');
+  if (res.headersSent) {
+    return next(error);
+  }
+
+  const { status, message } = error;
+
+  res.status(status || 500);
+  res.json({
+    error: {
+      message: message || 'Something went wrong!',
+    },
+  });
+});
+
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
 });
---
+++ backend/controller/places-controllers/getPlaceByUserId.js
@@ -0,0 +1,74 @@
+const DUMMY_PLACES = [
+  {
+    id: 1,
+    imageURL:
+      'https://media.istockphoto.com/photos/paris-aerial-panorama-with-river-seine-and-eiffel-tower-france-picture-id1336449613?b=1&k=20&m=1336449613&s=170667a&w=0&h=atFJsGNEMuHaPll6bRwOkZl8Q0Iz83EcUUi0SvhAeM8=',
+    placeName: 'Paris',
+    description:
+      'Paris is one of the most beautiful cities in the world. It is known worldwide for the Louvre Museum, Notre-Dame cathedral, and the Eiffel tower. It has a reputation of being a romantic and cultural city. The city is also known for its high-quality gastronomy and the terraces of its cafés',
+    address: {
+      country: 'France',
+      city: 'Paris',
+    },
+    location: {
+      lat: 48.8566,
+      lng: 2.3522,
+    },
+    rating: 4.5,
+    creator: 'user1',
+  },
+  {
+    id: 2,
+    imageURL:
+      'https://media.istockphoto.com/photos/shore-of-alexandria-egypt-picture-id157316325?b=1&k=20&m=157316325&s=170667a&w=0&h=qIojlEvA4WbM-LTXhInTzN-kEKGqb7S1uWmqtLC2wwY=',
+    placeName: 'Alexandria',
+    description:
+      'One of Egypts largest cities, Alexandria is also its principal seaport and a major industrial centre. The city lies on the Mediterranean Sea at the western edge of the Nile River delta, about 114 miles (183 km) northwest of Cairo in Lower Egypt. Area city, 116 square miles',
+    address: {
+      country: 'Egypt',
+      city: 'Alexandria',
+    },
+    location: {
+      lat: 31.2001,
+      lng: 29.9187,
+    },
+    rating: 0,
+    creator: 'user1',
+  },
+  {
+    id: 3,
+    imageURL:
+      'https://media.istockphoto.com/photos/nyhavn-copenhagen-denmark-picture-id901375804?b=1&k=20&m=901375804&s=170667a&w=0&h=SjhoV9MfiKSfJ4JVT7y62sfjlpq-OUqfjEhJwMlZQTY=',
+    placeName: 'Copenhagen',
+    description:
+      'Copenhagen is a unique city, characterized by its canals, cycling culture, strong economy, and happy locals. It is actually known as being the happiest city in the world, due to its shorter workdays, free college tuition, more vacation days, and levels of personal interaction.',
+    address: {
+      country: 'Denmark',
+      city: 'Copenhagen',
+    },
+    location: {
+      lat: 55.6761,
+      lng: 12.5683,
+    },
+    rating: 1.5,
+    creator: 'user2',
+  },
+];
+
+const getPlaceByUserId = async (req, res, next) => {
+  const { userId } = req.params;
+  const places = DUMMY_PLACES.filter((place) => place.creator === userId);
+
+  console.log(places);
+
+  if (places.length === 0) {
+    const error = new Error('Places not found!');
+    error.status = 404;
+    // throw error;
+    return next(error);
+  }
+
+  res.status(200).json({ places });
+};
+
+module.exports = getPlaceByUserId;
