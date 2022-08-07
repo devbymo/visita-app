65c217d40784f0b4330e1d99f198abd578413fcb
Mo Yasser <alalmy475@gmail.com>
2022-08-07 23:30:51 +0200

    users controllers init

---
+++ backend/controllers/users-controllers.js
@@ -0,0 +1,9 @@
+const getUsers = async (req, res, next) => {};
+const signup = async (req, res, next) => {};
+const login = async (req, res, next) => {};
+
+module.exports = {
+  getUsers,
+  signup,
+  login,
+};
